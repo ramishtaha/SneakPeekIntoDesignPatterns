fc174ef38e4bdaae535b4db54de7d49345eb85fe
Ramish Taha <ramishtaha1@gmail.com>
2024-05-04 03:08:02 +0530

    a

--- lambdastrategy/BetterRefactored.java
+++ lambdastrategy/BetterRefactored.java
@@ -3,14 +3,23 @@ import java.util.List;
 import java.util.function.Predicate;
 
 public class BetterRefactored {
-    public static int betterSumByTest(List<Integer> nums, Predicate<Integer> selector){
+    public static int betterSumByTest(List<Integer> nums,Boolean And, List<Predicate<Integer>> selectors){
+        Predicate<Integer> combinedPredicate = selectors.stream().reduce(Predicate::or).orElse(x -> false);
+        if (And) {
+            combinedPredicate = selectors.stream().reduce(Predicate::and).orElse(x -> true);
+        }
         return nums.stream()
-            .filter(selector)
+            .filter(combinedPredicate)
+            .mapToInt(Integer::intValue).sum();
+    }
+    public static int betterSumByTest(List<Integer> nums, Predicate<Integer> selectors){
+        return nums.stream()
+            .filter(selectors)
             .mapToInt(Integer::intValue).sum();
     }
     public static void main(String[] args) {
         System.out.println(betterSumByTest(List.of(1,2,3,4,5,6,7,8,9,10,11,13,14,15), NumberTests::passAll));
-        System.out.println(betterSumByTest(List.of(1,2,3,4,5,6), NumberTests::isEven));
-        System.out.println(betterSumByTest(List.of(1,2,3,4,5,6,7,8,9,10,11,13,14,15), NumberTests::isEven));
+        System.out.println(betterSumByTest(List.of(1,2,3,4,5,6,7,8,9,10,11,13,14,15), false, List.of(NumberTests::isEven, NumberTests::isOdd)));
+        System.out.println(betterSumByTest(List.of(1,2,3,4,5,6,7,8,9,10,11,13,14,15), true, List.of(NumberTests::isEven, NumberTests::isOdd)));
     }
 }
